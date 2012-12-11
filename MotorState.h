9f5177c53eb688fd310a6cc4d7c769be49bb32d6
Danny Gratzer <danny.gratzer@gmail.com>
2012-12-10 20:53:46 -0600

    Added MotorState POD

---
+++ MotorState.h
@@ -0,0 +1,15 @@
+#ifndef MOTOR_STATE_H
+#define MOTOR_STATE_H
+
+struct MotorState{
+  double topLeftPower;
+  double topLeftDirection;
+  double topRightPower;
+  double topRightDirection;
+  double lowerLeftPower;
+  double lowerLeftDirection;
+  double lowerRightPower;
+  double lowerRightDirection;
+};
+
+#endif
