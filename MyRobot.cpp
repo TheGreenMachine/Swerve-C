06fb08576f3b8918bda25dfa0efb1c87432f1e82
Danny Gratzer <danny.gratzer@gmail.com>
2012-12-11 18:43:03 -0600

    Gutted MyRobot.cpp. Currently does nothing.

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,45 +1,43 @@
 #include "WPILib.h"
+#include "JoystickState.h"
+#include "MotorState.h"
 
-/**
- * This is a demo program showing the use of the RobotBase class.
- * The SimpleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- */ 
 class RobotDemo : public SimpleRobot
 {
-	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+        Jaguar upperLeftJag;
+        Jaguar upperRightJag;
+        Jaguar lowerLeftJag;
+        Jaguar lowerRightJag;
+	Joystick stickLeft; 
+	Joystick stickRight;
 
 public:
 	RobotDemo(void):
-		myRobot(1, 2),	// these must be initialized in the same order
-		stick(1)		// as they are declared above.
-	{
-		myRobot.SetExpiration(0.1);
-	}
+          upperLeftJag(1),
+          upperRightJag(2),
+          lowerLeftJag(3),
+          lowerRightJag(4),
+          stickLeft(1),
+          stickRight(2)
+	{}
 
 	/**
-	 * Drive left & right motors for 2 seconds then stop
+	 * Do nothing
 	 */
 	void Autonomous(void)
 	{
-		myRobot.SetSafetyEnabled(false);
-		myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
+          //DO NOTHING
 	}
 
 	/**
-	 * Runs the motors with arcade steering. 
+	 * Swerve Drive
 	 */
 	void OperatorControl(void)
 	{
-		myRobot.SetSafetyEnabled(true);
 		while (IsOperatorControl())
 		{
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
-			Wait(0.005);				// wait for a motor update time
+                  //Magic
+                  Wait(0.005);				// wait for a motor update time
 		}
 	}
 };
